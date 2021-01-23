0c607d99ee604073d40e6cb36df9f396a88bd84a
halit <oskan96@gmail.com>
2021-01-23 20:25:38 +0000

    started workin on actions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React from "react";
+import { useSelector } from "react-redux";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello</div>;
+  const counter = useSelector((state) => state.counterReducer);
+  const isLoggedIn = useSelector((state) => state.loggedReducer);
+  return (
+    <div className="App">
+      <h1>Counter {counter}</h1>
+      <button>+</button>
+      <button>--</button>
+      {isLoggedIn && <h3>Members only</h3>}
+    </div>
+  );
 }
 
 export default App;
